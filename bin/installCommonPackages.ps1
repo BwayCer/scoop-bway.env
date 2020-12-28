327f3d795a588c20543529545c8ae28f02223a27
BwayCer <bwaycer@gmail.com>
2020-12-28 15:47:02 +0800

    temp 2020-12-28T07:47:02.462039000Z

--- bin/installCommonPackages.ps1
+++ bin/installCommonPackages.ps1
@@ -1,8 +1,11 @@
 # 安裝常用程式包
 
 
+scoop bucket add extras
 scoop bucket add bway https://github.com/BwayCer/scoop-bway.env
 
+scoop install main/ffmpeg
+scoop install extras/typora
 scoop install bway/dotnet-desktop-runtime@3.1.10
 scoop install bway/powertoys
 scoop install bway/ultimate-windows-context-menu-customizer

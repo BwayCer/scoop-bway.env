dbbc1d1758ea07ec54496f0f1004923036f1b1b4
BwayCer <bwaycer@gmail.com>
2022-10-20 18:16:02 +0800

    feat: 優化Scoop Portable邏輯

--- bin/scoop.portable.ps1
+++ bin/scoop.portable.ps1
@@ -1,10 +1,10 @@
 # Scoop 可攜版
 
 
-param($Path)
+param($portableScoopPath)
 
 # 未指定路徑 ;
-if ($Path -eq $null) {
+if ($portableScoopPath -eq $null) {
   throw "Not found `-Path` option."
 }
 
@@ -13,56 +13,40 @@ if ($args.Length -lt 1) {
   throw "No command ?"
 }
 
-# 未發現 Scoop 命令
-$origEnvScoopPath = $env:SCOOP
-if (-not (Test-Path "$origEnvScoopPath\shims") -Or -not (Test-Path "$origEnvScoopPath\apps\scoop")) {
-  throw "Not found `scoop` command."
+
+$originEnvPATH = $env:PATH
+$originEnvSCOOP = $env:SCOOP
+
+if (-not (Test-Path $portableScoopPath)) {
+  New-Item $portableScoopPath -ItemType Directory | Out-Null
+}
+if (-not (Test-Path "$portableScoopPath\buckets")) {
+  New-Item "$portableScoopPath\buckets" `
+    -ItemType Junction -Target "$originEnvSCOOP\buckets" | Out-Null
 }
 
-# 是否為新的 Scoop 安裝路徑
-if (-not (Test-Path "$Path\shims") -Or -not (Test-Path "$Path\apps\scoop")) {
-  $runTimes = 0
-  do {
-    try {
-      uname | Out-Null
-      echo "Is to use ""$Path"" path [Y/N]:"
-      $isUseThePath = Read-Host "input"
-    } catch {
-      $isUseThePath = Read-Host "Is to use ""$Path"" path [Y/N]"
-    }
-    if ($isUseThePath -match "^(y|Y|yes|Yes)$") {
-      break
-    } elseif ($isUseThePath -match "^(n|N|no|No)$") {
-      exit
-    }
-    if ($runTimes -ge 2) {
-      echo "Please provide `-Path` options."
-      exit
-    }
-    $runTimes += 1
-  } until (false)
-
-  if (-not (Test-Path "$Path\shims")) {
-    New-Item "$Path\shims" -ItemType Directory | Out-Null
-  }
-  if (-not (Test-Path "$Path\apps\scoop")) {
-    if (-not (Test-Path "$Path\apps")) {
-      New-Item "$Path\apps" -ItemType Directory | Out-Null
-    }
-    Copy-Item "$origEnvScoopPath\apps\scoop" "$Path\apps" -Force -Recurse
-  }
-  if (-not (Test-Path "$Path\buckets\bway")) {
-    if (-not (Test-Path "$Path\buckets")) {
-      New-Item "$Path\buckets" -ItemType Directory | Out-Null
-    }
-    Copy-Item "$PSScriptRoot\.." "$Path\buckets\bway" -Force -Recurse
-  }
-}
 
 try {
-  $env:SCOOP = $Path
-  powershell.exe scoop $args
+  # 避免被覆寫.
+  $zzzzzzOriginEnvPATH = $originEnvPATH
+  $zzzzzzOriginEnvSCOOP = $originEnvSCOOP
+
+  $env:PATH = "$portableScoopPath\shims;" + $originEnvPATH
+  $env:SCOOP = $portableScoopPath
+
+  switch ($args[0]) {
+    { @("install", "uninstall", "list") -contains $_ } {
+      $originUserEnvPATH = [Environment]::GetEnvironmentVariable('PATH', 'User')
+      scoop @args
+      [Environment]::SetEnvironmentVariable('PATH', $originEnvPATH, 'User')
+      break
+    }
+    default {
+      @args
+    }
+  }
 } finally {
-  $env:SCOOP = $origEnvScoopPath
+  $env:PATH = $zzzzzzOriginEnvPATH
+  $env:SCOOP = $zzzzzzOriginEnvSCOOP
 }
 

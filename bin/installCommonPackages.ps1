d5260ae400500cd41a9bf11a3226592c7a63e175
BwayCer <bwaycer@gmail.com>
2022-10-21 16:25:54 +0800

    feat: 移除非主要文件

--- bin/installCommonPackages.ps1
+++
@@ -1,8 +0,0 @@
-# 安裝常用程式包
-
-
-scoop bucket add java
-scoop bucket add bway https://github.com/BwayCer/scoop-bway.env
-
-scoop install bway/ultimate-windows-context-menu-customizer
-
--- test/portableCommand-01/bin/pkgCommand.ps1
+++
@@ -1,5 +0,0 @@
-portable-pkg.ps1 exportEnv | ForEach-Object { Invoke-Expression $_ }
-if (!(Test-Path Variable:PSScriptRoot)) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-$path = join-path "$PSScriptRoot" "..\..\..\test\windowsExecutive\TinyPE.exe"
-if($myinvocation.expectingInput) { $input | & $path @args } else { & $path @args }
-
